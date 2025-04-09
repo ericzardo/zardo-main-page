a6e85ceb644c2d08d514eb31304f27118f5a77d6
Eric Zardo <95961864+ericzardo@users.noreply.github.com>
2025-04-09 18:28:49 -0300

    optimizate loading screen use

--- src/app/[...not-found]/page.tsx
+++ src/app/[...not-found]/page.tsx
@@ -1,7 +1,18 @@
 "use client";
 
-import { NotFoundScreen } from "@zardo/ui-kit/feedback";
+import { useState, useEffect } from "react";
+import { NotFoundScreen, LoadingScreen } from "@zardo/ui-kit/feedback";
 
 export default function NotFoundPage() {
+  const [isClient, setIsClient] = useState(false);
+  
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return <LoadingScreen />;
+  }
+  
   return <NotFoundScreen backHref="/" />;
 }
